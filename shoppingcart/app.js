a74dbab1b673019f034a391d5688c4078cb29161
Amit Kumar Garg <akgarg@amadeus.com>
2016-10-25 09:28:13 +0530

    Formatting data with Filters

--- shoppingcart/app.js
+++ shoppingcart/app.js
@@ -73,3 +73,10 @@ myapp.controller("MainCtrl", function ($scope, ItemService) {
 
   $scope.priceClass = 'price-class';
 });
+
+myapp.filter('upperCase', function(){
+  //filters should be stateless
+  return function(input){
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+})
